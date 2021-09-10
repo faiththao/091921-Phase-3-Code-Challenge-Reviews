6aac9d723f721c56a20a9ea72729d407a62b5a22
faiththao <79170369+faiththao@users.noreply.github.com>
2021-09-10 11:39:25 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,54 @@ product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
 
 puts "Creating reviews..."
-# *****************************************************************
-# * TODO: create reviews! Remember, a review belongs to a product *
-# * and a review belongs to an user.                              *
-# *****************************************************************
-# Create Reviews Here
+p1r = Review.create(star_rating: 4, comment: "Works well but is scary to use.", product_id: product1.id, user_id: user1.id)
+p2r = Review.create(star_rating: 5, comment: "Super easy to use.", product_id: product2.id, user_id: user1.id)
+
 
 puts "Seeding done!"
+
+puts
+puts "Review#user for product1"
+puts p1r.user.name
+puts
+puts "Review#product for product1"
+puts p1r.product.name
+puts
+puts "Product#reviews/users for product1"
+puts product1.reviews.map(&:star_rating)
+puts product1.reviews.map(&:comment)
+puts product1.users.map(&:name)
+puts
+puts "User#reviews/products for product1"
+puts user1.reviews.map(&:star_rating)
+puts user1.reviews.map(&:comment)
+puts user1.products.map(&:name)
+puts 
+puts "Review#print_review"
+puts p1r.print_review
+puts
+puts "Product#leave_review"
+p1r2 = product1.leave_review(user2, 3, "Hard to push down on.")
+puts p1r2.print_review
+puts
+pro1 = Product.find_by_name("Stapler")
+puts pro1.reviews.map(&:star_rating)
+puts pro1.reviews.map(&:comment)
+puts pro1.users.map(&:name)
+puts
+puts "Print all reviews"
+puts pro1.print_all_reviews
+puts
+puts "Average rating"
+puts pro1.average_rating
+puts
+# puts "Fav Product"
+# puts user1.favorite_product.map(&:name)
+puts
+puts "remove review"
+puts user1.remove_reviews(product1)
+u1 = User.find_by_variable()
+puts u1.print_all_reviews
+
+
+puts
