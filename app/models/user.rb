207655fdf040aae40bb8bf18fc036c13c32660df
faiththao <79170369+faiththao@users.noreply.github.com>
2021-09-10 11:40:20 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def favorite_product
+        self.products
+        .filter {|product| product.star_rating}.last
+    end
+
+    def remove_reviews(product)
+        review = self.reviews.find_by_product_id(product.id)
+        if reviews != nil 
+            review.destroy
+        end
+    end
 end
