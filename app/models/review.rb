7e064c0cc3515e99423b02d3a46758cafa902d2a
faiththao <79170369+faiththao@users.noreply.github.com>
2021-09-10 11:41:19 -0600

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,1 +1,9 @@
+class Review < ActiveRecord::Base
+    belongs_to :product
+    belongs_to :user
 
+    def print_review
+        "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
+    end
+
+end
